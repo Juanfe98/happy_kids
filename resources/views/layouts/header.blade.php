bda3d75d69cfc358ec19553ca10957182cf1f867
Juan Felipe <juanmontana1398@gmail.com>
2019-08-24 18:55:40 -0500

    Home is working fine with the incorporation of slider

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>@yield('title')</title>
+    <title>Happy Kids</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -15,18 +15,17 @@
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link rel="stylesheet" href="css/magnific-popup.css">
+
     <link rel="stylesheet" href="css/aos.css">
 
     <link rel="stylesheet" href="css/ionicons.min.css">
 
     <link rel="stylesheet" href="css/flaticon.css">
-
     <link rel="stylesheet" href="css/icomoon.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    {{-- Begin upside bar  --}}
     <div class="py-2 bg-primary">
         <div class="container">
             <div class="row no-gutters d-flex align-items-start align-items-center px-3 px-md-0">
@@ -34,50 +33,45 @@
                     <div class="row d-flex">
                         <div class="col-md-5 pr-4 d-flex topper align-items-center">
                             <div class="icon bg-fifth mr-2 d-flex justify-content-center align-items-center"><span class="icon-map"></span></div>
-                            <span class="text">198 West 21th Street, Suite 721 New York NY 10016</span>
+                            <!--						    <span class="text">Condominio Bello Horizonte, Etapa 6B, Casa 41 </span>-->
                         </div>
                         <div class="col-md pr-4 d-flex topper align-items-center">
                             <div class="icon bg-secondary mr-2 d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
-                            <span class="text">youremail
-                                @email.com</span>
+                            <span class="text">happy_kids
+                                @hotmail.com</span>
                         </div>
                         <div class="col-md pr-4 d-flex topper align-items-center">
                             <div class="icon bg-tertiary mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
-                            <span class="text">+ 1235 2355 98</span>
+                            <span class="text">888-8430</span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    {{-- End upside bar --}}
-
-    {{-- Begin nav bar --}}
+    <!--    start nav-bar-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco_navbar ftco-navbar-light" id="ftco-navbar">
         <div class="container d-flex align-items-center">
-            <a class="navbar-brand" href="{{url(route('home')) }}">
-                <img src="images/logo3_nombre.png" alt="">
-            </a>
-
+            <a class="navbar-brand" href="index.html">Happy Kids</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="oi oi-menu"></span> Menu
             </button>
-
             <div class="collapse navbar-collapse" id="ftco-nav">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active"><a href="index.html" class="nav-link pl-0">Inicio</a></li>
-                    {{-- <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-                    <li class="nav-item"><a href="teacher.html" class="nav-link">Teacher</a></li>
-                    <li class="nav-item"><a href="courses.html" class="nav-link">Courses</a></li>
-                    <li class="nav-item"><a href="pricing.html" class="nav-link">Pricing</a></li>
-                    <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-                    <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li> --}}
+                    <li class="nav-item active"><a href="index.html" class="nav-link pl-0">Home</a></li>
+                    <!--
+	        	<li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+	        	<li class="nav-item"><a href="teacher.html" class="nav-link">Teacher</a></li>
+	        	<li class="nav-item"><a href="courses.html" class="nav-link">Courses</a></li>
+	        	<li class="nav-item"><a href="pricing.html" class="nav-link">Pricing</a></li>
+	        	<li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
+	          <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+-->
                 </ul>
             </div>
         </div>
     </nav>
     <!-- END nav -->
-
 
 
     @yield('content')
--- resources/views/pages/inicio.blade.php
+++ resources/views/pages/inicio.blade.php
@@ -30,6 +30,44 @@
     </div>
 </section>
 
+<section class="ftco-services ftco-no-pb">
+    <div class="container-wrap">
+        <div class="row no-gutters">
+            <div class="col-md-4 d-flex services align-self-stretch pb-4 px-4 ftco-animate bg-primary">
+                <div class="media block-6 d-block text-center">
+                    <div class="icon d-flex justify-content-center align-items-center">
+                        <span class="flaticon-reading"></span>
+                    </div>
+                    <div class="media-body p-2 mt-3">
+                        <h3 class="heading">Asesoria de Tareas</h3>
+                        <p>Acompañamiento directo a nuestros niños para realizar positivamente las labores académicas que se presentan a diario. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex services align-self-stretch pb-4 px-4 ftco-animate bg-tertiary">
+                <div class="media block-6 d-block text-center">
+                    <div class="icon d-flex justify-content-center align-items-center">
+                        <span class="flaticon-books"></span>
+                    </div>
+                    <div class="media-body p-2 mt-3">
+                        <h3 class="heading">Refuerzos</h3>
+                        <p>Clases de repaso para primaria, secundaria y clases de preparación de exámanes para todas las asignaturas.
+                            Aquí se reforzará en áreas donde existan falencias.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 d-flex services align-self-stretch pb-4 px-4 ftco-animate bg-fifth">
+                <div class="media block-6 d-block text-center">
+                    <div class="icon d-flex justify-content-center align-items-center">
+                        <span class="flaticon-kids"></span>
+                    </div>
+                    <div class="media-body p-2 mt-3">
+                        <h3 class="heading">Vacaciones Recreativas</h3>
+                        <p> Es un programa recreo-deportivo con actividades increíblemente divertidas para niños que desean disfrutar al máximo sus vacaciones</p>
+                    </div>
+                </div>
+            </div>
+</section>
 {{-- Slider Section Began --}}
 
 @endsection
